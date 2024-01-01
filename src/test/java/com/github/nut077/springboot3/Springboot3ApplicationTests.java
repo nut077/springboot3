736074b486c04c4d35dac66dbb453a30e0ff044b
nut077 <nuttapon.tavee@gmail.com>
2024-01-01 18:43:47 +0700

    Initial commit

---
+++ src/test/java/com/github/nut077/springboot3/Springboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nut077.springboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
