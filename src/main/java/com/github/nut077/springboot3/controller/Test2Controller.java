ef07f88a4197effe08e4e89437a098851c76a3ea
NuttapoT <nuttapon.t@fwd.com>
2024-06-28 13:48:15 +0700

    add test2 in branch2

---
+++ src/main/java/com/github/nut077/springboot3/controller/Test2Controller.java
@@ -0,0 +1,13 @@
+package com.github.nut077.springboot3.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test2Controller {
+
+  @GetMapping("/test2")
+  public String test() {
+    return "test1";
+  }
+}
